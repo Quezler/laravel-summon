d64fd9265d8cc2a4b56461c9661fa979f3c6469c
Quezler <Quezler@me.com>
2016-09-28 13:05:50 +0200

    Initialise ServiceProvider

---
+++ src/SummonServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Quezler\Laravel_Summon;
+
+use Illuminate\Support\ServiceProvider;
+
+class SummonServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot() {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
