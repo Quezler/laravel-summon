c4b4e23d6d3a2ca156f9f3c59acf99480daea16e
Quezler <Quezler@me.com>
2016-09-28 13:57:55 +0200

    Copy ConsoleSupportServiceProvider if no yet summoned

--- src/Console/SummonConsole.php
+++ src/Console/SummonConsole.php
@@ -40,15 +40,27 @@ class SummonConsole extends Command
     public function handle()
     {
         // possible locations of ArtisanServiceProvider
-        $provider_summon = base_path('app/Providers/')            . 'ArtisanServiceProvider.php';
-        $provider_vendor = base_path('vendor/laravel/framework/') . 'src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php';
+        $console_provider_summon = base_path('app/Providers/')            . 'ConsoleSupportServiceProvider.php';
+        $console_provider_vendor = base_path('vendor/laravel/framework/') . 'src/Illuminate/Foundation/Providers/ConsoleSupportServiceProvider.php';
 
-        if (file_exists($provider_summon)) {
+        if (file_exists($console_provider_summon)) {
+            $this->info('ConsoleSupportServiceProvider found in app/Providers.');
+        } else {
+            $this->comment('ConsoleSupportServiceProvider not found in app/Providers.');
+
+            File::copy($console_provider_vendor, $console_provider_summon);
+        }
+
+        // possible locations of ArtisanServiceProvider
+        $artisan_provider_summon = base_path('app/Providers/')            . 'ArtisanServiceProvider.php';
+        $artisan_provider_vendor = base_path('vendor/laravel/framework/') . 'src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php';
+
+        if (file_exists($artisan_provider_summon)) {
             $this->info('ArtisanServiceProvider found in app/Providers.');
         } else {
             $this->comment('ArtisanServiceProvider not found in app/Providers.');
 
-            File::copy($provider_vendor, $provider_summon);
+            File::copy($artisan_provider_vendor, $artisan_provider_summon);
         }
     }
 }
