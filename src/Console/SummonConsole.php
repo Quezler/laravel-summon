d248c0de73183ea3b38219a2abd905c4d2527e85
Quezler <Quezler@me.com>
2016-09-28 13:49:23 +0200

    Copy ArtisanServiceProvider if no yet summoned

--- src/Console/SummonConsole.php
+++ src/Console/SummonConsole.php
@@ -4,6 +4,7 @@ namespace Quezler\Laravel_Summon\Console;
 
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class SummonConsole extends Command
 {
@@ -38,6 +39,16 @@ class SummonConsole extends Command
      */
     public function handle()
     {
-        //
+        // possible locations of ArtisanServiceProvider
+        $provider_summon = base_path('app/Providers/')            . 'ArtisanServiceProvider.php';
+        $provider_vendor = base_path('vendor/laravel/framework/') . 'src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php';
+
+        if (file_exists($provider_summon)) {
+            $this->info('ArtisanServiceProvider found in app/Providers.');
+        } else {
+            $this->comment('ArtisanServiceProvider not found in app/Providers.');
+
+            File::copy($provider_vendor, $provider_summon);
+        }
     }
 }
