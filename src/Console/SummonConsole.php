970dc8d8ad9f038e2cb82455b40842166d3233b4
Quezler <Quezler@me.com>
2016-09-28 15:44:41 +0200

    Turn file patch functionality into function

--- src/Console/SummonConsole.php
+++ src/Console/SummonConsole.php
@@ -61,63 +61,34 @@ class SummonConsole extends Command
         $this->line(''); // spacer
 
         // update namespace of summoned ConsoleSupportServiceProvider
-        $file = file_get_contents($console_provider_summon);
-        if (Str::contains($file, 'namespace App\Providers;')) {
-            $this->info('ConsoleSupportServiceProvider namespace has been updated.');
-        } else {
-            $this->comment('ConsoleSupportServiceProvider namespace has not yet been updated.');
-            $file = str_replace(
-                'namespace Illuminate\Foundation\Providers;',
-                'namespace App\Providers;',
-                $file
-            );
-            file_put_contents($console_provider_summon, $file);
-        }
+        $this->patch(
+            $console_provider_summon,
+            'namespace Illuminate\Foundation\Providers;',
+            'namespace App\Providers;'
+        );
 
         // update namespace of summoned ArtisanServiceProvider
-        $file = file_get_contents($artisan_provider_summon);
-        if (Str::contains($file, 'namespace App\Providers;')) {
-            $this->info('ArtisanServiceProvider namespace has been updated.');
-        } else {
-            $this->comment('ArtisanServiceProvider namespace has not yet been updated.');
-            $file = str_replace(
-                'namespace Illuminate\Foundation\Providers;',
-                'namespace App\Providers;',
-                $file
-            );
-            file_put_contents($artisan_provider_summon, $file);
-        }
+        $this->patch(
+            $artisan_provider_summon,
+            'namespace Illuminate\Foundation\Providers;',
+            'namespace App\Providers;'
+        );
 
         $this->line(''); // spacer
 
         // update ArtisanServiceProvider pointer in summoned ConsoleSupportServiceProvider
-        $file = file_get_contents($console_provider_summon);
-        if (Str::contains($file, 'App\Providers\ArtisanServiceProvider')) {
-            $this->info('ConsoleSupportServiceProvider\'s ArtisanServiceProvider pointer has been updated.');
-        } else {
-            $this->comment('ConsoleSupportServiceProvider\'s ArtisanServiceProvider pointer has not yet been updated.');
-            $file = str_replace(
-                'Illuminate\Foundation\Providers\ArtisanServiceProvider',
-                'App\Providers\ArtisanServiceProvider',
-                $file
-            );
-            file_put_contents($console_provider_summon, $file);
-        }
+        $this->patch(
+            $console_provider_summon,
+            'Illuminate\Foundation\Providers\ArtisanServiceProvider',
+            'App\Providers\ArtisanServiceProvider'
+        );
 
         // update ConsoleSupportServiceProvider pointer in config.app
-        $config_app = base_path('config/') . 'app.php';
-        $file = file_get_contents($config_app);
-        if (Str::contains($file, 'App\Providers\ConsoleSupportServiceProvider::class')) {
-            $this->info('config.app\'s ConsoleSupportServiceProvider pointer has been updated.');
-        } else {
-            $this->comment('config.app\'s ConsoleSupportServiceProvider pointer has not yet been updated.');
-            $file = str_replace(
-                'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class',
-                'App\Providers\ConsoleSupportServiceProvider::class',
-                $file
-            );
-            file_put_contents($config_app, $file);
-        }
+        $this->patch(
+            base_path('config/') . 'app.php',
+            'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class',
+            'App\Providers\ConsoleSupportServiceProvider::class'
+        );
 
         $this->line(''); // spacer
         $this->question("if you see this, you're good to go! (っ◕‿◕)っ");
@@ -147,6 +118,23 @@ class SummonConsole extends Command
     }
 
     private function patch($filePath, $search, $replace) {
+        $file = file_get_contents($filePath);
+
+        if (Str::contains($file, $replace)) {
+            $this->info("$filePath contains [$replace]");
+        } else {
+            $this->comment("$filePath contains [$search]");
+            $this->comment("Updating file...");
+
+            $file = str_replace(
+                $search,
+                $replace,
+                $file
+            );
+
+            file_put_contents($filePath, $file);
+            $this->patch($filePath, $search, $replace);
+        }
 
     }
 
